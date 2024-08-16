215da2109e7460fde90969848bb0e0b1f930a6aa
Dan Lowe <dan@danlowe.me>
2024-08-15 21:25:26 -0400

    if done

--- exercises/02_functions/functions5.rs
+++ exercises/02_functions/functions5.rs
@@ -1,6 +1,8 @@
 // TODO: Fix the function body without changing the signature.
 fn square(num: i32) -> i32 {
-    num * num;
+    let res = num * num;
+    res
+    // or could just be `num * num`
 }
 
 fn main() {
